243d3e381ea1dcc4ce65ba4ac6c4e3f832dd0485
dranandpandey <64311367+dranandpandey@users.noreply.github.com>
2022-02-25 14:14:55 +0530

    changed main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from wsgiref import simple_server
+from flask import Flask
+from flask import Response
+app = Flask(__name__)
+
+
+
+@app.route("/train", methods=['GET']) # Changed from POST
+
+def trainRouteClient():
+
+    try:
+        path = 'Training_Batch_Files'
+
+    except Exception as e:
+
+        return Response("Error Occurred! %s" % e)
+
+    return Response("Training successfull!!")
+
+
+
+port = int(os.getenv("PORT",5003))
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    httpd = simple_server.make_server(host, port, app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
